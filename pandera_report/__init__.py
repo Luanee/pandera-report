89df925d753e54de27b6c24cd2b0f6bb9a871501
Pit Nahrstedt <pit_nahrstedt@web.de>
2023-09-22 20:50:43 +0200

    🔖 Release version 0.1.0

--- pandera_report/__init__.py
+++ pandera_report/__init__.py
@@ -3,11 +3,17 @@
 from pandera_report.options import QualityColumnsOptions, QualityStatusOptions
 from pandera_report.parser import DefaultFailureCaseParser, FailureCaseParser
 from pandera_report.validator import DataFrameValidator
+from pandera_report.version import __version__
 
 __all__ = [
+    # validator
+    "DataFrameValidator",
+    # parser
     "DefaultFailureCaseParser",
     "FailureCaseParser",
-    "DataFrameValidator",
+    # options
     "QualityStatusOptions",
     "QualityColumnsOptions",
+    # version
+    "__version__",
 ]
